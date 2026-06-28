8b2d0af01740ba552db5b8863af7f7ee689d14f9
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-06-28 18:10:35 +0530

    fourth c program

---
+++ basics/multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the no.");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    {
+        printf("multiply=%d",c);
+    }
+    return 0;
+} 
