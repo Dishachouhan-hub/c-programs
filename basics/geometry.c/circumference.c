1187f772e5e44bc9abae0592b84dc2e7861aed18
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-04 15:40:29 +0530

    eighth c program

---
+++ basics/geometry.c/circumference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    float circumference,num=2,pie=3.14,radius;
+    printf("enter the circumference of circle");
+    scanf("%f",&radius);
+    circumference= 2*3.14*radius;
+    {
+        printf("circum=%f",circumference);
+    }
+    return 0;
+}
