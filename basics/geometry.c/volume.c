e2e2305c55a73fcaf09ce9147292f244a1173a21
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-04 14:46:23 +0530

    seventh C program

---
+++ basics/geometry.c/volume.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    float volume , a;
+    printf("enter the volume");
+    scanf("%f",&a);
+    volume = a*a*a;
+    {
+        printf("volume=%f",volume);
+    }
+    return 0;
+}
