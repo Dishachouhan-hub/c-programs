33a2ad60a41b9a90817beef066923323a5c8823f
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-11 17:20:50 +0530

    tenth c program

---
+++ basics/if-else.c/odd-even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter the no.");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("number is even");
+    }
+    else 
+    {
+        printf("number is odd");
+    }
+    return 0;
+}
