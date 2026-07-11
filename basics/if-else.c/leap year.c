24d411e80ba196c23f80d64bba20a15db1c510b5
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-11 17:50:05 +0530

    thirteenth c program

---
+++ basics/if-else.c/leap year.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int year;
+    printf("enter your year");
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        printf("it is leap year");
+    }
+    else
+    {
+        printf("it is not a leap year");
+    }
+    return 0;
+}
