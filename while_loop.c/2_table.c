6377caad1a3f30111e388702e534e8a417b4ddba
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-25 21:59:14 +0530

    twenty one c program

---
+++ while_loop.c/2_table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("enter the no.");
+    scanf("%d",&n);
+    i=2;
+    while(i<=n)
+    {
+        printf("%d\n",i);
+        i=i+2;
+    }
+    return 0;
+}
