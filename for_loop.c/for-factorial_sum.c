1273ee442f3ec16575b4099bd7adf0309b811462
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-07-31 14:53:47 +0530

    twenty nine c program

---
+++ for_loop.c/for-factorial_sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n,f=0;
+    printf("enter the fact no.");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    f=f+i;
+    {
+        printf("factorial=%d",f);
+    }
+    return 0;
+}
