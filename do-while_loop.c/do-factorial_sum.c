73f8a8cddd805ec1cfe1c827325631d2aa97dd15
Dishachouhan-hub <disha.cse2448e@sdbc.ac.in>
2026-08-03 13:31:00 +0530

    thirty five c program

---
+++ do-while_loop.c/do-factorial_sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1,n,f=0;
+    printf("enter the no.");
+    scanf("%d",&n);
+    do
+    {
+        f=f+i;
+        i++;
+    }     
+    while(i<=n);
+    printf("%d",f);
+    return 0;
+}
